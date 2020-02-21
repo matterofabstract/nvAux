256845a2dc4cfd199544aeb974550059ed98f4ed
Ben Keating <ben@abstractly.io>
2020-02-21 14:44:31 -0600

    detect platform and os version utlity

--- public/utils.js
+++ public/utils.js
@@ -1,5 +1,3 @@
-const { app } = require('electron');
-
 const isDev = () => {
   const app = electron.app || electron.remote.app;
   const isEnvSet = 'ELECTRON_IS_DEV' in process.env;
@@ -7,4 +5,15 @@ const isDev = () => {
   return isEnvSet ? getFromEnv : !app.isPackaged;
 };
 
+const getSystemInfo = () => ({
+  // 'darwin', 'freebsd', 'linux', 'sunos' or 'win32'
+  platform: process.platform,
+
+  // On macOS -> '10.13.6'
+  // On Windows -> '10.0.17763'
+  // On Linux -> '4.15.0-45-generic'
+  version: process.getSystemVersion()
+});
+
 exports.isDev = isDev;
+exports.getSystemInfo = getSystemInfo;
