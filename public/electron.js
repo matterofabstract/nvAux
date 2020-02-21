135c5aa009a026e79b78ff0aa7d9282cfced90cd
Ben Keating <ben@abstractly.io>
2020-02-21 15:34:44 -0600

    disable window shadowing

--- public/electron.js
+++ public/electron.js
@@ -39,6 +39,7 @@ app.on('ready', () => {
       : `file://${path.join(__dirname, '../build/index.html')}`
   );
   // mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.setHasShadow(false);
 });
 
 app.on('window-all-closed', () => {
