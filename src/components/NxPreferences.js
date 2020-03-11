59047b3dc621a32c20860ebd07b4202a54a982b9
Ben Keating <ben@abstractly.io>
2020-03-10 22:17:19 -0500

    Working Directory preference settin

--- src/components/NxPreferences.js
+++ src/components/NxPreferences.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import * as Store from 'electron-store';
+const { dialog } = require('electron').remote;
 
 export const NxPreferences = () => {
+  const store = new Store();
+
+  const handleSetWorkingDir = () =>
+    console.log(
+      dialog
+        .showOpenDialog({ properties: ['openDirectory'] })
+        .then((result) => {
+          console.log(result.canceled);
+          store.set('workingDir', result.filePaths);
+          console.log(result.filePaths);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    );
   return (
     <div className="preferences-wrapper">
       <h1>nvAux Preferences</h1>
       <div>
+        <h3>Working Directory</h3>
+        <input type="text" style={{ width: 300 }} value={store.get('workingDir')} />{' '}
+        <button onClick={handleSetWorkingDir}>Set...</button>
+      </div>
+      <hr />
+      <div>
         <h3>Summon (Bring-to-Front) Hotkey</h3>
         <input type="text" style={{ width: 60 }} value="⌘." /> <button>Set...</button>
       </div>
-      <hr />
     </div>
   );
 };
