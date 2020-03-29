7f4450382fb3663ce0b0df488a20c2a825a46b65
Ben Keating <ben@abstractly.io>
2020-03-29 12:27:33 -0500

    added fdir, sqlite3 and electron-rebuild

--- public/electron.js
+++ public/electron.js
@@ -5,9 +5,13 @@
 require('./core/');
 
 const path = require('path');
+const Store = require('electron-store');
+const fdir = require('fdir');
 const { app, BrowserWindow } = require('electron');
 
 const { isDev } = require('./utils');
+
+const store = new Store();
 
 let mainWindow = null;
 
@@ -30,6 +34,11 @@ app.on('ready', () => {
   createWindow();
   mainWindow.loadURL(isDev() ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.setHasShadow(false);
+  fdir.async(store.get('workingDir')).then((props) =>
+    // Send to Render process...
+    console.log('1231232121313312', props)
+  );
+  const files = fdir.async(store.get('workingDir'));
 });
 
 app.on('window-all-closed', () => {
@@ -50,3 +59,22 @@ app.on('activate', () => {
     mainWindow.show();
   }
 });
+
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('/Users/bpk/Downloads/db.sqlite3');
+
+db.serialize(function() {
+  db.run('CREATE TABLE lorem (info TEXT)');
+
+  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
+  for (var i = 0; i < 10; i++) {
+    stmt.run('Ipsum ' + i);
+  }
+  stmt.finalize();
+
+  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
+    console.log(row.id + ': ' + row.info);
+  });
+});
+
+db.close();
