858891ee5c364b3284647298c4edb0c650375fb2
Ben Keating <ben@abstractly.io>
2020-04-14 13:46:35 -0500

    lifted db out into core, changed default max dimensions

--- public/electron.js
+++ public/electron.js
@@ -23,10 +23,10 @@ function createWindow() {
     frame: false,
     transparent: true,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     },
-    minWidth: 100,
-    minHeight: 100
+    minWidth: 200,
+    minHeight: 280,
   });
 }
 
@@ -59,22 +59,3 @@ app.on('activate', () => {
     mainWindow.show();
   }
 });
-
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('/Users/bpk/Downloads/db.sqlite3');
-
-db.serialize(function() {
-  db.run('CREATE TABLE lorem (info TEXT)');
-
-  var stmt = db.prepare('INSERT INTO lorem VALUES (?)');
-  for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i);
-  }
-  stmt.finalize();
-
-  db.each('SELECT rowid AS id, info FROM lorem', function(err, row) {
-    console.log(row.id + ': ' + row.info);
-  });
-});
-
-db.close();
