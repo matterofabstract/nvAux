568cc85501d84c1be13574a69fe72755ee0b3177
Ben Keating <ben@abstractly.io>
2021-01-27 00:48:20 -0600

    showPreferences state

--- src/store.js
+++ src/store.js
@@ -6,8 +6,13 @@ export const StoreContext = React.createContext();
 export const StoreProvider = ({ children }) => {
 
   const store = useLocalStore(() => ({
+    showPreferences: localStorage.getItem('showPreferences') || false,
     omniText: localStorage.getItem('omniText') || '',
 
+    setShowPreferences: () => {
+      store.showPreferences = !store.showPreferences;
+      localStorage.setItem('showPreferences', store.showPreferences);
+    },
     setOmniText: (searchString) => {
       store.omniText = searchString;
       localStorage.setItem('omniText', searchString);
