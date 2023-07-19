c589224013750718fe0040ac74df0936a2902ab6
Benjamin Keating <ben@abstractly.io>
2023-07-18 21:40:33 -0500

    save point

--- src/App.svelte
+++ src/App.svelte
@@ -4,12 +4,22 @@
   import ResizeHandle from './lib/ResizeHandle.svelte';
   import NoteDetail from './lib/NoteDetail.svelte';
   import StatusBar from './lib/StatusBar.svelte';
+
+  import { fullScreen } from './lib/store';
 </script>
 
-<main class="w-screen h-screen overflow-hidden flex flex-col">
-  <OmniBar />
-  <NoteList />
-  <ResizeHandle />
-  <NoteDetail />
-  <StatusBar />
-</main>
+<div class="h-screen w-screen flex flex-col justify-center items-center {$fullScreen ? '' : 'p-2'}">
+  {#if !$fullScreen}
+    <div style="text-align: center;">
+      <h1 style=" font-size: 40px; font-weight: 600">nvAux</h1>
+      <p style="max-width: 500px; margin-bottom: 60px;">Capture and retrieve ideas at the speed of thought with nvAux, the in-the-zone, aint-nobody-stopping-me note-taking app for creative professionals.</p>
+    </div>
+  {/if}
+  <main class="relative overflow-hidden flex flex-col" style="transition: all 0.5s ease-in-out; {$fullScreen ? 'height: 100%; width: 100%; border-color: transparent;' : 'width: 690px; min-width: 320px; height: 50%; min-height: 400px; border-color: #2d2f30; border-radius: 8px; -webkit-box-shadow: 0px 36px 69px -24px rgba(0,0,0,0.75); -moz-box-shadow: 0px 36px 69px -24px rgba(0,0,0,0.75); box-shadow: 0px 36px 69px -24px rgba(0,0,0,0.75);'} background-color: #181a1c; border: 1px solid; overflow: hidden;">
+    <OmniBar />
+    <NoteList />
+    <ResizeHandle />
+    <NoteDetail />
+    <StatusBar />
+  </main>
+</div>
--- src/lib/StatusBar.svelte
+++ src/lib/StatusBar.svelte
@@ -1,3 +1,30 @@
-<div class="px-2 flex items-center absolute w-full flex-grow-0" style="height: 30px; background: #181a1c; bottom: 0; left: 0; color: #606060; ">
-nvAux v0.1.5
+<script>
+  import { onMount } from 'svelte';
+  import { format } from 'date-fns';
+  import { fullScreen } from './store';
+
+  let time = new Date();
+
+  onMount(() => {
+    const interval = setInterval(() => {
+      time = new Date();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+</script>
+
+<div
+  class="px-2 flex items-center absolute w-full flex-grow-0"
+  style="font-size: 12px; height: 35px; background: #181a1c; bottom: 0; left: 0; color: #606060; "
+>
+  <div class="flex-grow">nvAux v0.1.5</div>
+  <div>
+    <button on:click={() => $fullScreen = !$fullScreen} style="margin-right: 10px; color: #ed0178" class="bg-transparent">
+      [&nbsp;{$fullScreen ? '*' : ' '}&nbsp;]
+    </button>
+  </div>
+  <div>{format(time, 'hh:mm:ss a')}</div>
 </div>
